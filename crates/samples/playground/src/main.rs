f2a0bacab8c0ea3e27312ed9cc43c2e62ac6ed6d
Osei Fortune <fortune.osei@yahoo.com>
2023-06-02 03:29:22 -0400

    feat: playground

---
+++ crates/samples/playground/src/main.rs
@@ -0,0 +1,116 @@
+use std::ffi::CString;
+use node_buffer::{Buffer, StringEncoding};
+
+fn main() {
+    let mut buffer = Buffer::builder()
+        .size(12)
+        .build();
+
+    buffer.fill(CString::new("Hello World").unwrap(), None);
+
+
+    println!("data {:?}", buffer.buffer());
+
+
+    let buffer1: &[u8] = &[1, 2, 3];
+    let buffer2: &[u8] = &[4, 5, 6];
+    let buffer3: &[u8] = &[7, 8, 9];
+
+
+    let buffer = Buffer::concat(&[buffer1, buffer2, buffer3], Some(0));
+
+    println!("data {:?}", buffer.buffer());
+
+
+    let buffer = Buffer::builder()
+        .size(11)
+        .fill_text(CString::new("aGVsbG8gd29ybGQ=").unwrap(), StringEncoding::Base64)
+        .build();
+
+    println!("{:?}", buffer.buffer());
+
+
+    let mut buffer = Buffer::builder()
+        .size(26)
+        .build();
+
+    let buf = buffer.buffer_mut();
+
+    for i in 0..26 as usize {
+        buf[i] = (i + 97) as u8;
+    }
+
+
+    println!("{}", buffer.to_string(Some(StringEncoding::Utf8), None, None));
+// Prints: abcdefghijklmnopqrstuvwxyz
+    println!("{}", buffer.to_string(Some(StringEncoding::Utf8), Some(0), Some(5)));
+// Prints: abcde
+
+
+    let buffer = Buffer::from_slice(&[0x12, 0x34, 0x56, 0x78]);
+
+    println!("{:x}", buffer.read_uint32le(Some(0)));
+// Prints: 78563412
+    // println!("{:x}",buffer.read_uint32le(Some(1)).unwrap());
+// Throws ERR_OUT_OF_RANGE.
+
+
+    let mut buffer = Buffer::builder()
+        .size(8)
+        .build();
+
+    buffer.write_big_int64be(0x0102030405060708, Some(0));
+
+    println!("{:?}", buffer.buffer());
+
+
+    let mut buffer = Buffer::builder()
+        .size(8)
+        .build();
+
+    buffer.write_big_int64le(0x0102030405060708, Some(0));
+
+    println!("{:?}", buffer.buffer());
+
+
+    let mut buffer = Buffer::builder()
+        .size(8)
+        .build();
+
+    buffer.write_big_uint64be(0xdecafafecacefade, Some(0));
+
+    println!("{:?}", buffer);
+
+
+    let mut buffer = Buffer::builder()
+        .size(8)
+        .build();
+
+    buffer.write_big_uint64le(0xdecafafecacefade, Some(0));
+
+    println!("{:?}", buffer);
+
+
+    let mut buffer = Buffer::builder()
+        .size(4)
+        .build();
+
+    buffer.write_uint8(0x3, Some(0));
+    buffer.write_uint8(0x4, Some(1));
+    buffer.write_uint8(0x23, Some(2));
+    buffer.write_uint8(0x42, Some(3));
+
+    println!("{:?}", buffer);
+// Prints: <Buffer 03 04 23 42>
+
+
+    let mut buffer = Buffer::builder()
+        .size(4)
+        .build();
+
+   // buffer.write_int16be(0xdead, Some(0));
+   // buffer.write_int16be(0xbeef, Some(2));
+
+    println!("{}", buffer);
+// Prints: <Buffer de ad be ef>
+}
