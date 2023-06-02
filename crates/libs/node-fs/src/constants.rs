2ddf8bb6a0b862e3e55a2a81b7b392875cb8bd7f
Osei Fortune <fortune.osei@yahoo.com>
2023-06-02 15:35:52 -0400

    chore: handle windows

--- crates/libs/node-fs/src/constants.rs
+++ crates/libs/node-fs/src/constants.rs
@@ -20,6 +20,7 @@ pub static FILE_OPEN_OPTIONS_O_WRONLY: c_int = libc::O_WRONLY;
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_RDWR: c_int = libc::O_RDWR;
 
+
 #[cfg(any(target_os = "android"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_CREAT: c_int = libc::O_CREAT;
@@ -28,14 +29,25 @@ pub static FILE_OPEN_OPTIONS_O_CREAT: c_int = libc::O_CREAT;
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_CREAT: c_int = libc::O_CREAT;
 
+
+#[cfg(any(target_os = "windows"))]
+#[no_mangle]
+pub static FILE_OPEN_OPTIONS_O_CREAT: c_int = libc::O_CREAT;
+
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_EXCL: c_int = libc::O_EXCL;
+
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_NOCTTY: c_int = libc::O_NOCTTY;
+
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_TRUNC: c_int = libc::O_TRUNC;
+
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_APPEND: c_int = libc::O_APPEND;
+
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_DIRECTORY: c_int = libc::O_DIRECTORY;
 
@@ -47,10 +59,15 @@ pub static FILE_OPEN_OPTIONS_O_NOATIME: c_int = libc::MS_NOATIME as c_int;
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_NOATIME: c_int = libc::MNT_NOATIME;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_NOFOLLOW: c_int = libc::O_NOFOLLOW;
+
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_SYNC: c_int = libc::O_SYNC;
+
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_DSYNC: c_int = libc::O_DSYNC;
 
@@ -64,18 +81,24 @@ pub static FILE_OPEN_OPTIONS_O_SYMLINK: c_int = -1;
 
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_DIRECT: c_int = 0x4000;
+
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_OPEN_OPTIONS_O_NONBLOCK: c_int = libc::O_NONBLOCK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_ACCESS_OPTIONS_F_OK: c_int = libc::F_OK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_ACCESS_OPTIONS_R_OK: c_int = libc::R_OK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_ACCESS_OPTIONS_W_OK: c_int = libc::W_OK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_ACCESS_OPTIONS_X_OK: c_int = libc::X_OK;
 
@@ -91,50 +114,66 @@ pub static FILE_TYPE_OPTIONS_S_IFDIR: mode_t = libc::S_IFDIR;
 #[no_mangle]
 pub static FILE_TYPE_OPTIONS_S_IFCHR: mode_t = libc::S_IFCHR;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_TYPE_OPTIONS_S_IFBLK: mode_t = libc::S_IFBLK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_TYPE_OPTIONS_S_IFIFO: mode_t = libc::S_IFIFO;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_TYPE_OPTIONS_S_IFLNK: mode_t = libc::S_IFLNK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_TYPE_OPTIONS_S_IFSOCK: mode_t = libc::S_IFSOCK;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IRWXU: mode_t = libc::S_IRWXU;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IRUSR: mode_t = libc::S_IRUSR;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IWUSR: mode_t = libc::S_IWUSR;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IXUSR: mode_t = libc::S_IXUSR;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IRWXG: mode_t = libc::S_IRWXG;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IRGRP: mode_t = libc::S_IRGRP;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IWGRP: mode_t = libc::S_IWGRP;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IXGRP: mode_t = libc::S_IXGRP;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IRWXO: mode_t = libc::S_IRWXO;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IROTH: mode_t = libc::S_IROTH;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IWOTH: mode_t = libc::S_IWOTH;
 
+#[cfg(not(target_os = "windows"))]
 #[no_mangle]
 pub static FILE_MODE_OPTIONS_S_IXOTH: mode_t = libc::S_IXOTH;
